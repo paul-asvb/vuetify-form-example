d9ba1ff41dd94ff9b7b4265875850a77d0d29341
Paul <paul@paul-mayer.eu>
2021-02-19 12:37:35 +0100

    init sample form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+  <v-col class="roi-observation bg-color-dark-black-light">
+    <p class="color-white-light" style="margin-bottom:0px;">
+      chip group
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+    <v-chip-group :value="chipGroupItems" active-class="primary" column>
+      <v-chip
+        v-for="tag in chipGroupItems"
+        :key="tag.text"
+        color="black"
+        small
+        auto-select-first="true"
+        :value="tag.text"
+        @click="chipGroupItems = tag.text"
+      >
+        <span>{{ tag.text }}</span>
+      </v-chip>
+    </v-chip-group>
 
+    <p class="color-white-light" style="margin-bottom:0px;">
+      combobox
+    </p>
+    <v-combobox
+      v-model="comboBoxModel"
+      hide-details
+      :items="comboBoxItem"
+      multiple
+      :hide-selected="true"
+      allow-overflow
+      :menu-props="{ closeOnContentClick: true }"
+      chips
+      solo
+      flat
+      full-width
+      background-color="grey"
+      color="black"
+      item-color="black"
+    >
+      <template #selection="data">
+        <v-chip
+          :key="JSON.stringify(data.item)"
+          close
+          v-bind="data.attrs"
+          :input-value="data.selected"
+          :disabled="data.disabled"
+          color="blue"
+          small
+          @click:close="data.parent.selectItem(data.item)"
+        >
+          <span> {{ data.item }}</span>
+        </v-chip>
+      </template>
+    </v-combobox>
+  </v-col>
+</template>
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
+  data: () => ({
+comboBoxModel:null,
+    comboBoxItem: [],
+    chipGroupItems: [
+      {
+        text: "Hyperdense",
+        value: "hyperdense"
+      },
+      {
+        text: "Hypodense",
+        value: "hypodense"
+      },
+      {
+        text: "Isodense",
+        value: "isodense"
+      }
+    ]
+  }),
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
