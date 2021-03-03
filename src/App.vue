76f776dda51188d423aed01eda150f758ad2d1ad
tmcdos <tmcdos@abv.bg>
2021-03-03 14:59:33 +0200

    Updated the patch for VJSF to use v-combobox instead of v-autocomplete and removed the (x) from the chips in the example form definition.

--- src/App.vue
+++ src/App.vue
@@ -124,7 +124,7 @@ export default {
                 chipItemProps:
                   {
                     filter: true,
-                    close: true,
+                    // close: true,
                   }
               }
           },
